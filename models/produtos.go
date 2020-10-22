8dfb2ee8c439d1bae91c4770976691bb2e450c5e
Trondoli <daniel.de-paula-trondoli@dxc.com>
2020-10-22 17:42:35 -0300

    Commit Inicial

---
+++ models/produtos.go
@@ -0,0 +1,44 @@
+package models
+
+import "web_com_golang/db"
+
+// Struct Produtos
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func GetAllProducts() []Produto {
+	myDb := db.ConnectionDataBase()
+	defer myDb.Close()
+
+	allProducts, err := myDb.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	products := []Produto{}
+
+	for allProducts.Next() {
+		var id, qtd int
+		var name, description string
+		var price float64
+
+		err = allProducts.Scan(&id, &name, &description, &price, &qtd)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = name
+		p.Descricao = description
+		p.Preco = price
+		p.Quantidade = qtd
+
+		products = append(products, p)
+	}
+
+	return products
+}
